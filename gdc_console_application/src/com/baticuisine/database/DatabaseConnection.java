50d40d8c15f5b80cfefed94b0f2de6525885864a
voidgravity <abdellahbardichwork@gmail.com>
2024-09-20 03:00:38 +0100

    separation of concern || adding a database layer :fire:

---
+++ gdc_console_application/src/com/baticuisine/database/DatabaseConnection.java
@@ -0,0 +1,30 @@
+package com.baticuisine.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static DatabaseConnection instance;
+    private Connection connection;
+
+    private DatabaseConnection() {
+        try {
+            Class.forName("org.postgresql.Driver");
+            this.connection = DriverManager.getConnection("jdbc:postgresql://localhost:5444/baticuisine", "your_username", "your_password");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static synchronized DatabaseConnection getInstance() {
+        if (instance == null) {
+            instance = new DatabaseConnection();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+}
